c7012fb6c3efe43189cc35c100aaf34983f1fb4b
lock <srleo@qq.com>
2019-09-13 21:44:58 +0800

    20190913

---
+++ publictonpm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ./dist/
+
+rm demo.html
+
+cp ../src/player/danplayer.d.ts ./
+
+npm init --yes
+
+json -I -f package.json -e \
+"this.author='gzlock <gzlock88@gmail.com>';
+this.name='danplayer';
+this.version='$1';
+this.types='danplayer.d.ts';
+this.license='MIT'
+delete this.scripts
+"
+
