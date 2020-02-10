b4dfac397ac302be86d83a08168bd17e10e9e52d
gzlock <gzlock88@gmail.com>
2020-02-10 20:42:12 +0800

    0.0.29+dev

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
-const removeConsolePlugin = []
-if (process.env.NODE_ENV === 'production') {
-  removeConsolePlugin.push('transform-remove-console')
-}
+const plugins = []
+// if (process.env.NODE_ENV === 'production') {
+//   plugins.push('transform-remove-console')
+// }
 module.exports = {
   presets: [['@vue/app', { useBuiltIns: 'entry' }]],
-  plugins: removeConsolePlugin
+  plugins,
 }
