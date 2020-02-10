4c6590acfae4ad1e8a089b14fe9a30defde1afe1
gzlock <gzlock88@gmail.com>
2020-02-10 20:49:21 +0800

    0.0.29

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 const plugins = []
-// if (process.env.NODE_ENV === 'production') {
-//   plugins.push('transform-remove-console')
-// }
+if (process.env.NODE_ENV === 'production') {
+  plugins.push('transform-remove-console')
+}
 module.exports = {
   presets: [['@vue/app', { useBuiltIns: 'entry' }]],
   plugins,
